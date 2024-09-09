f146e4e4a9afa0ccf6e337a09491e772a3fa62e7
chris-newton <newton.chris685@gmail.com>
2024-09-09 12:57:00 -0400

    created index.html, style.css, and javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+for (i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    
+    for (j = 0; j < 16; j++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        row.appendChild(pixel);
+    }
+    
+    container.appendChild(row);
+}
+
