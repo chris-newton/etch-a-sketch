9b3722ec491241e42f478ca300fc02f5395a24ae
chris-newton <newton.chris685@gmail.com>
2024-09-09 13:14:01 -0400

    added eventlisteners so that "pixel" divs are painted on mouseover

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
-const container = document.querySelector(".container");
+function drawBoard() {
+    const container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    
-    for (j = 0; j < 16; j++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        row.appendChild(pixel);
+    for (i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        
+        for (j = 0; j < 16; j++) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            row.appendChild(pixel);
+        }
+        
+        container.appendChild(row);
     }
-    
-    container.appendChild(row);
 }
 
+drawBoard();
+
+document.querySelectorAll(".pixel").forEach((pixel) => {
+    pixel.addEventListener("mouseenter", () => {
+        pixel.classList.add("painted");
+    })
+});
